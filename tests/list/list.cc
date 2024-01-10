67a9efde9ee4841f1310744b867b755e1b233d7d
Spiros Maggioros <spirosastro@gmail.com>
2024-01-10 15:06:25 +0200

    updates with catch2 unit testing
    added catch2 - single header - unit testing framework to add tests for algorithms and classes.

--- tests/list/list.cc
+++ tests/list/list.cc
@@ -3,6 +3,8 @@
 #include "../catch2/catch.hpp"
 
 TEST_CASE("testing linked list") {
+  // this is an example of a unit test to check if any algo or class works as it
+  // should
   linked_list<int> l1({1, 4, 5, 10});
   REQUIRE(l1.search(4) == true);
   REQUIRE(l1.search(150) == false);
