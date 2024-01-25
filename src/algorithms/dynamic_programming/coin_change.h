865d4bd01e515ef0a2954e89881549e53d5e03c6
Spiros Maggioros <spirosastro@gmail.com>
2024-01-25 03:30:07 +0200

    tried to integrate cmake

--- src/algorithms/dynamic_programming/coin_change.h
+++ src/algorithms/dynamic_programming/coin_change.h
@@ -6,20 +6,20 @@
 #include <vector>
 #endif
 
-namespace knapsack{
-  int64_t min_cost(std::vector<int64_t> arr, int64_t N){
-    int max_of = N + 1;
-    std::vector<int> dp(N + 1 , max_of);
-    dp[0] = 0;
-    for(int i = 0; i<=N; i++){
-      for(auto & x : arr){
-        if(x <= i){
-          dp[i] = std::min(dp[i] , 1 + dp[i -x]);
-        }
+namespace knapsack {
+int64_t min_cost(std::vector<int64_t> arr, int64_t N) {
+  int max_of = N + 1;
+  std::vector<int> dp(N + 1, max_of);
+  dp[0] = 0;
+  for (int i = 0; i <= N; i++) {
+    for (auto &x : arr) {
+      if (x <= i) {
+        dp[i] = std::min(dp[i], 1 + dp[i - x]);
       }
     }
-    return dp[N] > N ? -1 : dp[N];
   }
-};
+  return dp[N] > N ? -1 : dp[N];
+}
+}; // namespace knapsack
 
 #endif
---
+++ src/include/algoplus.h
@@ -0,0 +1,16 @@
+#ifndef ALGOPLUS_H
+#define ALGOPLUS_H
+
+#include "../algorithms/dynamic_programming/coin_change.h"
+#include "../algorithms/string/min_distance.h"
+#include "../algorithms/number_theory/gcd.h"
+#include "../classes/heap/min_heap.h"
+#include "../classes/tree/avl_tree.h"
+#include "../classes/tree/bst.h"
+#include "../classes/tree/trie.h"
+#include "../classes/list/linked_list.h"
+#include "../classes/list/doubly_linked_list.h"
+#include "../classes/graph/graph.h"
+#include "../classes/disjoint_set/disjoint_set.h"
+
+#endif
