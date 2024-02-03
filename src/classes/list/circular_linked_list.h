6ed7a2985c64673bbb115f2e7c46d116f14a6a3b
Spiros Maggioros <spirosastro@gmail.com>
2024-02-03 06:16:15 +0200

    merged circular linked list and added iterators + test cases

--- src/classes/list/circular_linked_list.h
+++ src/classes/list/circular_linked_list.h
@@ -5,169 +5,280 @@
 #include <iostream>
 #endif
 
-template <typename T>
-class circular_linked_list
-{
+/**
+ * @brief circular linked list class
+ */
+template <typename T> class circular_linked_list {
 public:
-    explicit circular_linked_list(std::vector<T> __elements = {}) noexcept
-        : root(nullptr), tail(nullptr), __size(0)
-    {
-        if (!__elements.empty())
-        {
-            for (T &x : __elements)
-            {
-                this->push_back(x);
-            }
-        }
+  /**
+   * @brief Construct a new circular linked list object
+   *
+   * @param __elements vector<T> so you dont have to do multiple insertions
+   */
+  explicit circular_linked_list(std::vector<T> __elements = {}) noexcept
+      : root(nullptr), tail(nullptr), __size(0) {
+    if (!__elements.empty()) {
+      for (T &x : __elements) {
+        this->push_back(x);
+      }
     }
+  }
 
-    bool empty() { return root == nullptr; }
+  /**
+   * @brief empty function
+   *
+   * @return true if the list is empty
+   * @return false otherwise
+   */
+  bool empty() { return root == nullptr; }
 
-    size_t size() { return __size; }
+  /**
+   * @brief size function
+   *
+   * @return size_t the size of the list
+   */
+  size_t size() { return __size; }
 
-    void push_back(T key)
-    {
-        std::shared_ptr<node> p = std::make_shared<node>(key);
-        if (root == nullptr)
-        {
-            root = p;
-            tail = p;
-        }
-        else
-        {
-            tail->next = p;
-            tail = p;
-        }
-        tail->next = root; 
-        __size++;
-    }
+  class Iterator;
 
-    void push_front(T key)
-    {
-        std::shared_ptr<node> p = std::make_shared<node>(key);
-        if (root == nullptr)
-        {
-            root = p;
-            tail = p;
-        }
-        else
-        {
-            p->next = root;
-            root = p;
-            tail->next = root;
-        }
-        __size++;
-    }
+  /**
+   * @brief pointer that points to begin
+   *
+   * @return Iterator
+   */
+  Iterator begin() { return Iterator(root); }
 
-    void erase(T key)
-    {
-        if (empty())
-        {
-            return;
-        }
+  /**
+   * @brief pointer that points to end
+   *
+   * @return Iterator
+   */
+  Iterator end() { return Iterator(nullptr); }
 
-        std::shared_ptr<node> t = root;
-        std::shared_ptr<node> to_be_removed = nullptr;
+  /**
+   * @brief push_back function
+   *
+   * @param key the key to be inserted
+   */
+  void push_back(T key);
 
-        do
-        {
-            if (t->next->val == key)
-            {
-                to_be_removed = t->next;
-                t->next = t->next->next;
-                to_be_removed.reset();
-                __size--;
+  /**
+   * @brief push_front function
+   *
+   * @param key the key to be inserted
+   */
+  void push_front(T key);
 
-                if (t->next == root)
-                {
-                    tail = t; 
-                }
+  /**
+   * @brief erase function
+   *
+   * @param key the key to be erased
+   */
+  void erase(T key);
 
-                if (root == tail)
-                {
-                    tail = nullptr;
-                }
+  /**
+   * @brief search function
+   *
+   * @param key the key to be searched
+   * @return true if the key exist in the list
+   * @return false otherwise
+   */
+  bool search(T key);
 
-                return;
-            }
+  /**
+   * @brief elements function
+   *
+   * @return std::vector<T> the elements of the list
+   */
+  std::vector<T> elements();
 
-            t = t->next;
-        } while (t != root);
-    }
-
-    bool search(T key)
-    {
-        try
-        {
-            if (empty())
-            {
-                return false;
-            }
-            else
-            {
-                std::shared_ptr<node> t = root;
-                do
-                {
-                    if (t->val == key)
-                    {
-                        return true;
-                    }
-                    t = t->next;
-                } while (t != root);
-
-                return false;
-            }
-        }
-        catch (std::invalid_argument &e)
-        {
-            std::cerr << e.what() << '\n';
-            return false;
-        }
-    }
-
-    std::vector<T> elements()
-    {
-        std::vector<T> __elements;
-
-        if (this->empty())
-        {
-            return __elements;
-        }
-
-        std::shared_ptr<node> head = root;
-        do
-        {
-            __elements.push_back(head->val);
-            head = head->next;
-        } while (head != root);
-
-        return __elements;
-    }
-
-    friend std::ostream &operator<<(std::ostream &out, circular_linked_list<T> &l1)
-    {
-        out << '{';
-        std::shared_ptr<node> head = l1.root;
-        do
-        {
-            out << head->val << ' ';
-            head = head->next;
-        } while (head != l1.root);
-        out << '}' << '\n';
-        return out;
-    }
+  /**
+   * @brief << operator for the circular list class
+   */
+  friend std::ostream &operator<<(std::ostream &out,
+                                  circular_linked_list<T> &l1) {
+    out << '{';
+    std::shared_ptr<node> head = l1.root;
+    do {
+      out << head->val << ' ';
+      head = head->next;
+    } while (head != l1.root);
+    out << '}' << '\n';
+    return out;
+  }
 
 private:
-    struct node
-    {
-        T val;
-        std::shared_ptr<node> next;
-        node(T key = 0) : val(key), next(nullptr) {}
-    };
+  /**
+   * @brief struct for the node
+   * @param val the value of the node
+   * @param next smart pointer to the next node
+   */
+  struct node {
+    T val;
+    std::shared_ptr<node> next;
+    node(T key = 0) : val(key), next(nullptr) {}
+  };
 
-    std::shared_ptr<node> root;
-    std::shared_ptr<node> tail;
-    size_t __size;
+  std::shared_ptr<node> root;
+  std::shared_ptr<node> tail;
+  size_t __size;
 };
 
+template <typename T> void circular_linked_list<T>::push_back(T key) {
+  std::shared_ptr<node> p = std::make_shared<node>(key);
+  if (root == nullptr) {
+    root = p;
+    tail = p;
+  } else {
+    tail->next = p;
+    tail = p;
+  }
+  tail->next = root;
+  __size++;
+}
+
+template <typename T> void circular_linked_list<T>::push_front(T key) {
+  std::shared_ptr<node> p = std::make_shared<node>(key);
+  if (root == nullptr) {
+    root = p;
+    tail = p;
+  } else {
+    p->next = root;
+    root = p;
+    tail->next = root;
+  }
+  __size++;
+}
+
+template <typename T> void circular_linked_list<T>::erase(T key) {
+  if (empty()) {
+    return;
+  }
+
+  std::shared_ptr<node> t = root;
+  std::shared_ptr<node> to_be_removed = nullptr;
+
+  do {
+    if (t->next->val == key) {
+      to_be_removed = t->next;
+      t->next = t->next->next;
+      to_be_removed.reset();
+      __size--;
+
+      if (t->next == root) {
+        tail = t;
+      }
+
+      if (root == tail) {
+        tail = nullptr;
+      }
+
+      return;
+    }
+
+    t = t->next;
+  } while (t != root);
+}
+
+template <typename T> bool circular_linked_list<T>::search(T key) {
+  try {
+    if (empty()) {
+      return false;
+    } else {
+      std::shared_ptr<node> t = root;
+      do {
+        if (t->val == key) {
+          return true;
+        }
+        t = t->next;
+      } while (t != root);
+
+      return false;
+    }
+  } catch (std::invalid_argument &e) {
+    std::cerr << e.what() << '\n';
+    return false;
+  }
+}
+
+template <typename T> std::vector<T> circular_linked_list<T>::elements() {
+  std::vector<T> __elements;
+
+  if (this->empty()) {
+    return __elements;
+  }
+
+  std::shared_ptr<node> head = root;
+  do {
+    __elements.push_back(head->val);
+    head = head->next;
+  } while (head != root);
+
+  return __elements;
+}
+/**
+ * @brief Iterator class
+ */
+template <typename T> class circular_linked_list<T>::Iterator {
+private:
+  std::shared_ptr<node> curr_root;
+
+public:
+  /**
+   * @brief Construct a new Iterator object
+   *
+   * @param l linked list type
+   */
+  explicit Iterator(const std::shared_ptr<node> &l) noexcept : curr_root(l) {}
+
+  /**
+   * @brief = operator for Iterator type
+   *
+   * @param current smart pointer of type node
+   * @return Iterator&
+   */
+  Iterator &operator=(std::shared_ptr<node> current) {
+    this->curr_root = current;
+    return *(this);
+  }
+
+  /**
+   * @brief operator ++ for type Iterator
+   *
+   * @return Iterator&
+   */
+  Iterator &operator++() {
+    if (curr_root) {
+      curr_root = curr_root->next;
+    }
+    return *(this);
+  }
+
+  /**
+   * @brief operator ++ for type Iterator
+   *
+   * @return Iterator
+   */
+  Iterator operator++(int) {
+    Iterator it = *this;
+    ++*(this);
+    return it;
+  }
+
+  /**
+   * @brief operator != for type Iterator
+   *
+   * @param it const Iterator
+   * @return true if curr_root == it.curr_root
+   * @return false otherwise
+   */
+  bool operator!=(const Iterator &it) { return curr_root != it.curr_root; }
+
+  /**
+   * @brief operator * for type Iterator
+   *
+   * @return T the value of the node
+   */
+  T operator*() { return curr_root->val; }
+};
 #endif
---
+++ tests/list/circular_linked_list.cc
@@ -0,0 +1,80 @@
+#define CATCH_CONFIG_MAIN
+#include "../../src/classes/list/circular_linked_list.h"
+#include "../catch2/catch.hpp"
+#include <string>
+
+TEST_CASE("testing search") {
+  // this is an example of a unit test to check if any algo or class works as it
+  // should
+  circular_linked_list<int> l1({1, 4, 5, 10});
+  REQUIRE(l1.search(4) == true);
+  REQUIRE(l1.search(150) == false);
+}
+
+TEST_CASE("push front") {
+  circular_linked_list<int> l1;
+  l1.push_front(1);
+  l1.push_front(2);
+  l1.push_front(5);
+  std::vector<int> v = {5, 2, 1};
+  std::vector<int> elem = l1.elements();
+  REQUIRE(v == elem);
+}
+
+TEST_CASE("testing linked list with strings") {
+  std::vector<std::string> __first = {"hello", "world"};
+  circular_linked_list<std::string> l1(__first);
+  REQUIRE(l1.search("hello") == true);
+  l1.push_back("check");
+  REQUIRE(l1.search("chec") == false); // needs to be fixed
+}
+
+TEST_CASE("testing linked list with chars") {
+  std::vector<char> __first = {'a', 'b', 'c', 'd'};
+  circular_linked_list<char> l1(__first);
+  l1.erase('b');
+  std::vector<char> __second = {'a', 'c', 'd'};
+  std::vector<char> elem = l1.elements();
+  REQUIRE(elem == __second);
+  l1.push_back('g');
+  __second.push_back('g');
+  elem = l1.elements();
+  REQUIRE(elem == __second);
+}
+
+TEST_CASE("testing iteration") {
+  circular_linked_list<char> l1({'a', 'b', 'c', 'd'});
+  std::vector<char> ans = {'a', 'b', 'c', 'd'};
+  std::vector<char> elem = l1.elements();
+  REQUIRE(elem == ans);
+}
+
+// TEST_CASE("testing reverse") {
+//   circular_linked_list<int> l1;
+//   l1.push_back(10);
+//   l1.push_back(11);
+//   l1.push_back(12);
+//   l1.push_back(13);
+//   l1.reverse();
+//   std::vector<int> elem = l1.elements();
+//   std::vector<int> v = {13, 12, 11, 10};
+//   REQUIRE(elem == v);
+//
+//   circular_linked_list<std::string> l2({"hello", "world", "universe"});
+//   l2.reverse();
+//   std::vector<std::string> els = l2.elements();
+//   std::vector<std::string> v2 = {"universe", "world", "hello"};
+//   REQUIRE(els == v2);
+// }
+
+TEST_CASE("testing iterators") {
+  circular_linked_list<int> l({4, 7, 1, 2, 3, 41, 32});
+  std::vector<int> els = {4, 7, 1, 2, 3, 41, 32, 4, 7, 1, 2, 3, 41, 32};
+  std::vector<int> v;
+  int counter = 0;
+  for (auto it = l.begin(); counter < 14 && it != l.end(); it++) {
+    v.push_back(*(it));
+    counter++;
+  }
+  REQUIRE(v == els);
+}
