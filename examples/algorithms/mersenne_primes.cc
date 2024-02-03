16772726d96c57934de4728fd195718830af298c
Spiros Maggioros <spirosastro@gmail.com>
2024-02-03 12:20:52 +0200

    minor change

--- examples/algorithms/mersenne_primes.cc
+++ examples/algorithms/mersenne_primes.cc
@@ -4,8 +4,8 @@
 
 int main() {
   int64_t n = 100;
-  std::vector<int> first_10_mersenne_primes = mersenne(10000000);
-  for (auto &x : first_10_mersenne_primes) {
+  std::vector<int> first_10000000_mersenne_primes = mersenne(10000000);
+  for (auto &x : first_10000000_mersenne_primes) {
     std::cout << x << " ";
   }
 }
