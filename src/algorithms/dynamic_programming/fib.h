f5577fb119a88d2f787e7b0900e2faed07338617
Spiros Maggioros <spirosastro@gmail.com>
2024-02-04 06:34:05 +0200

    added fibonacci algorithms

---
+++ src/algorithms/dynamic_programming/fib.h
@@ -0,0 +1,67 @@
+#ifndef FIB_H
+#define FIB_H
+
+#ifdef __cplusplus
+#include <iostream>
+#include <vector>
+#endif
+
+/**
+ * @brief fibonacci function
+ *
+ * @param n upper boumd
+ * @return int64_t returns the total fibonacci numbers till n
+ */
+int64_t fibonacci(int64_t n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+/**
+ * @brief fibonacci with dp function
+ *
+ * @param n upper boumd
+ * @return int64_t returns the total fibonacci numbers till n
+ */
+int64_t fibonacci_dp(int64_t n) {
+  std::vector<int64_t> dp(n + 2);
+  dp[0] = 0;
+  dp[1] = 1;
+  for (int64_t i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+}
+
+/**
+ * @brief fibonacci with bottom up dp function
+ *
+ * @param n upper boumd
+ * @return int64_t returns the total fibonacci numbers till n
+ */
+int64_t fibonacci_bottom_up(int64_t n) {
+  int64_t a = 0, b = 1, c;
+  if (n == 0) {
+    return 0;
+  }
+  for (int64_t i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+
+/**
+ * @brief fibonacci using binet's formula(golden ratio) function
+ *
+ * @param n upper boumd
+ * @return int64_t returns the total fibonacci numbers till n
+ */
+int64_t fibonacci_binet(int64_t n) {
+  double phi = (std::sqrt(5) + 1) / 2;
+  return std::round(std::pow(phi, n) / sqrt(5));
+}
+#endif
---
+++ tests/algorithms/dynamic_programming/fibonacci.cc
@@ -0,0 +1,29 @@
+#define CATCH_CONFIG_MAIN
+#include "../../../src/algorithms/dynamic_programming/fib.h"
+#include "../../catch2/catch.hpp"
+
+TEST_CASE("testing fibonacci functions") {
+  int64_t n1 = 2, ans1 = 1;
+  int64_t n2 = 4, ans2 = 3;
+  int64_t n3 = 29, ans3 = 514229;
+  int64_t n4 = 15, ans4 = 610;
+  REQUIRE(fibonacci(n1) == ans1);
+  REQUIRE(fibonacci_dp(n1) == ans1);
+  REQUIRE(fibonacci_bottom_up(n1) == ans1);
+  REQUIRE(fibonacci_binet(n1) == ans1);
+
+  REQUIRE(fibonacci(n2) == ans2);
+  REQUIRE(fibonacci_dp(n2) == ans2);
+  REQUIRE(fibonacci_bottom_up(n2) == ans2);
+  REQUIRE(fibonacci_binet(n2) == ans2);
+
+  REQUIRE(fibonacci(n3) == ans3);
+  REQUIRE(fibonacci_dp(n3) == ans3);
+  REQUIRE(fibonacci_bottom_up(n3) == ans3);
+  REQUIRE(fibonacci_binet(n3) == ans3);
+
+  REQUIRE(fibonacci(n4) == ans4);
+  REQUIRE(fibonacci_dp(n4) == ans4);
+  REQUIRE(fibonacci_bottom_up(n4) == ans4);
+  REQUIRE(fibonacci_binet(n4) == ans4);
+}
