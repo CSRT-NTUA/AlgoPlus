d41782320f0d136f102a260d1c1208fe2c92f432
Floyd <69324179+Shu-AFK@users.noreply.github.com>
2024-02-04 14:11:06 +0100

    Create merge_sort.h
    merge_sort functionality

---
+++ src/algorithms/sorting/merge_sort.h
@@ -0,0 +1,27 @@
+#ifndef ALGOPLUS_MERGE_SORT_H
+#define ALGOPLUS_MERGE_SORT_H
+
+#include <iostream>
+#include <vector>
+#include <algorithm> // Required for std::inplace_merge
+
+/**
+ * @brief Performs a merge sort on a range of elements.
+ * @param begin An iterator to the beginning of the range to be sorted.
+ * @param end An iterator to the end of the range to be sorted.
+ * @details This function sorts the elements in the range [begin, end) into ascending order.
+ * The sort is performed in-place. It divides the range into two halves, recursively sorts each half,
+ * and then merges the two sorted halves together. The function requires that the range of elements
+ * is sortable by the criteria used in std::inplace_merge.
+ */
+template <typename Iter>
+void merge_sort(Iter begin, Iter end) {
+    if (end - begin > 1) {
+        Iter mid = begin + (end - begin) / 2; // Assumes Random Access Iterator
+        merge_sort(begin, mid);
+        merge_sort(mid, end);
+        std::inplace_merge(begin, mid, end); // Elements must be comparable and move-assignable
+    }
+}
+
+#endif //ALGOPLUS_MERGE_SORT_H
