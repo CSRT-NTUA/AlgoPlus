062a70cee73889948651ecdd9627009b8a8fd984
Spiros Maggioros <spirosastro@gmail.com>
2024-02-04 23:02:12 +0200

    minor changes to quicksort tests

--- tests/algorithms/sorting/quick_sort.cc
+++ tests/algorithms/sorting/quick_sort.cc
@@ -4,7 +4,7 @@
 #include <random>
 #include <vector>
 
-TEST_CASE("testing merge sort") {
+TEST_CASE("testing quick sort") {
     std::vector<int64_t> v;
     for (int i = 0; i < 5000; i++) {
         int random = rand() % 50000;
@@ -12,4 +12,15 @@ TEST_CASE("testing merge sort") {
     }
     quick_sort(v.begin(), v.end());
     REQUIRE(std::is_sorted(v.begin(), v.end()) == true);
+}
+
+TEST_CASE("testing quick sort 2"){
+  std::vector<char> v;
+  std::vector<char> characters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+  for(int i = 0; i<10000; i++){
+    int random = rand() % 25;
+    v.push_back(characters[random]);
+  }
+  quick_sort(v.begin(), v.end());
+  REQUIRE(std::is_sorted(v.begin() , v.end()) == true);
 }
