12405c8328e794407be7faf5af147cda946bacb5
Spiros Maggioros <spirosastro@gmail.com>
2024-02-06 12:12:29 +0200

    some minor changes

--- src/algorithms/sorting/heap_sort.h
+++ src/algorithms/sorting/heap_sort.h
@@ -2,55 +2,57 @@
 #define HEAP_SORT_H
 
 #ifdef __cplusplus
+#include <cstdint>
 #include <iostream>
 #include <vector>
 #endif
 
-template<typename T> static void heapify_method(std::vector<T> &arr, int arrayLength, int parent) {
-    
-    //largest node
-    int largest = parent;
+/**
+ * @brief heapify method
+ * @param arr input array
+ * @param parent parent of the index
+ */
+template <typename T>
+static void heapify_method(std::vector<T> &arr, int64_t n, int64_t parent) {
 
-    //left child
-    int left = 2* parent +1;
+  // largest node
+  int64_t largest = parent;
 
-    //right child 
-    int right = 2* parent +2;
+  // left child
+  int64_t left = 2 * parent + 1;
 
-    // Compare left child with parent
-    if(left < arrayLength && arr[left] > arr[largest])
-    {
-        largest = left;
-    }
-    if(right < arrayLength && arr[right] > arr[largest])
-    {
-        largest = right;
-    }
-    if (largest != parent)
-    {
-        std::swap(arr[parent], arr[largest]);
+  // right child
+  int64_t right = 2 * parent + 2;
 
-        heapify_method(arr, arrayLength, largest);
-    }
+  // Compare left child with parent
+  if (left < n && arr[left] > arr[largest]) {
+    largest = left;
+  }
+  if (right < n && arr[right] > arr[largest]) {
+    largest = right;
+  }
+  if (largest != parent) {
+    std::swap(arr[parent], arr[largest]);
+
+    heapify_method(arr, n, largest);
+  }
 }
 
 /**
  * @brief heap sort function
  * @param arr input array
- * @param arrayLength input length array
  */
-template<typename T> void heap_sort(std::vector<T> &arr, int arrayLength) {
-    //heapify the array at first
-    for(int i = arrayLength/2 -1; i>=0; i--)
-    {
-        heapify_method(arr, arrayLength, i);
-    }
-    for(int i = arrayLength-1; i>=0; i--)
-    {
-        std::swap(arr[0],arr[i]);
+template <typename T> void heap_sort(std::vector<T> &arr) {
+  int64_t n = arr.size();
+  // heapify the array at first
+  for (int64_t i = n / 2 - 1; i >= 0; i--) {
+    heapify_method(arr, n, i);
+  }
+  for (int64_t i = n - 1; i >= 0; i--) {
+    std::swap(arr[0], arr[i]);
 
-        heapify_method(arr, i, 0);
-    }
+    heapify_method(arr, i, 0);
+  }
 }
 
 #endif
