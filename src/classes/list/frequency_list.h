5b02f34c91e953d559d83492ca079d3458662907
Spiros Maggioros <spirosastro@gmail.com>
2024-02-19 19:24:40 +0200

    everything seems to work!

--- src/classes/list/frequency_list.h
+++ src/classes/list/frequency_list.h
@@ -24,7 +24,7 @@
 template<typename T>
 class frequency_list {
 public:
-    /**
+  /**
      * @brief Constructs a frequency_list object with an optional initializer list of elements.
      *
      * The frequency_list object is constructed with an optional initializer list of elements.
@@ -34,16 +34,16 @@ public:
      * @tparam T The type of elements stored in the frequency_list.
      * @param data An optional initializer list of elements.
      */
-    explicit frequency_list(std::vector<T> data = {}) noexcept
-        : head(nullptr), tail(nullptr), size(0) {
-        if(!data.empty()) {
-            for (const auto &item: data) {
-                this->push_back(item);
-            }
-        }
+  explicit frequency_list(std::vector<T> data = {}) noexcept
+    : head(nullptr), tail(nullptr), size(0) {
+    if(!data.empty()) {
+      for (const auto &item: data) {
+        this->push_back(item);
+      }
     }
+  }
 
-    /**
+  /**
      * @brief Copy constructor for the frequency_list class.
      *
      * This constructor creates a new frequency_list object by copying the elements from another frequency_list object.
@@ -51,15 +51,15 @@ public:
      *
      * @param list The frequency_list object to copy from.
      */
-    frequency_list(const frequency_list<T> &list) : head(nullptr), tail(nullptr), size(0) {
-        for (auto iter = list.head; iter != nullptr; iter = iter->next) {
-            this->push_back(iter->data);
-        }
+  frequency_list(const frequency_list<T> &list) : head(nullptr), tail(nullptr), size(0) {
+    for (auto iter = list.head; iter != nullptr; iter = iter->next) {
+      this->push_back(iter->data);
     }
+  }
 
-    class Iterator;
+  class Iterator;
 
-    /**
+  /**
      * @brief Adds an element to the back of the frequency list.
      *
      * This function adds an element to the back of the frequency list. If the frequency list is empty,
@@ -69,10 +69,10 @@ public:
      *
      * @param data The data to be added to the frequency list.
      */
-    void push_back(T data);
+  void push_back(T data);
 
 
-    /**
+  /**
      * @brief Adds an element to the front of the frequency list.
      *
      * If the frequency list is not empty, it updates the previous pointer
@@ -81,9 +81,9 @@ public:
      *
      * @param data The data to be added to the frequency list.
      */
-    void push_front(T data);
+  void push_front(T data);
 
-    /**
+  /**
      * @brief Searches for a given key in the frequency list.
      *
      * This function searches for a given key in the frequency list. If the key is found,
@@ -93,18 +93,18 @@ public:
      * @param key The key to search for in the frequency list.
      * @return Returns true if the key is found and the frequency is incremented, false otherwise.
      */
-    bool search(T key);
+  bool search(T key);
 
-    /**
+  /**
      * @brief Gets the frequency of a key in the frequency list.
      *
      * @param key The key to get the frequency of.
      * @return The frequency of the key, or -1 if the key is not found.
      */
-    int64_t get_frequency(T key);
+  int64_t get_frequency(T key);
 
 
-    /**
+  /**
      * @brief Removes the first occurrence of a given key in the frequency list.
      *
      * If the key is found, it removes the corresponding node and updates the pointers.
@@ -113,31 +113,31 @@ public:
      *
      * @param key The key to be removed from the frequency list.
      */
-    void erase(T key);
+  void erase(T key);
 
-    /**
+  /**
      * @brief Resets the frequency of all nodes in the frequency list to 1.
      */
-    void reset_frequency();
+  void reset_frequency();
 
 
-    /**
+  /**
      * @brief Checks if the frequency list is empty.
      *
      * @return Returns true if the frequency list is empty, false otherwise.
      */
-    bool empty() { return head == nullptr; }
+  bool empty() { return head == nullptr; }
 
-    /**
+  /**
      * @brief Get an iterator pointing to the beginning of the frequency list.
      *
      * The Iterator provides a way to traverse the frequency list.
      *
      * @return An Iterator object pointing to the beginning of the frequency list.
      */
-    Iterator begin() { return Iterator(head); }
+  Iterator begin() { return Iterator(head); }
 
-    /**
+  /**
      * @brief Get an iterator pointing to the end of the frequency list.
      *
      * The end iterator is used to indicate the position after the last element of the frequency list,
@@ -145,9 +145,9 @@ public:
      *
      * @return An Iterator object pointing to the end of the frequency list.
      */
-    Iterator end() { return Iterator(nullptr); }
+  Iterator end() { return Iterator(nullptr); }
 
-    /**
+  /**
      * @brief Overloaded output stream insertion operator for the frequency_list class.
      *
      * This operator allows the frequency_list object to be printed to the output stream.
@@ -158,34 +158,34 @@ public:
      * @param flist The frequency_list object to be printed.
      * @return The output stream.
      */
-    friend std::ostream& operator<<(std::ostream& os, const frequency_list<T>& flist) {
-        auto iterator = flist.head;
-        while (iterator != nullptr) {
-            os << iterator->data << "(" << iterator->freq << ") ";
-            iterator = iterator->next;
-        }
-        return os;
+  friend std::ostream& operator<<(std::ostream& os, const frequency_list<T>& flist) {
+    auto iterator = flist.head;
+    while (iterator != nullptr) {
+      os << iterator->data << "(" << iterator->freq << ") ";
+      iterator = iterator->next;
     }
+    return os;
+  }
 
 private:
-    struct node {
-        T data;
-        int64_t freq;
-        std::shared_ptr<node> next;
-        std::weak_ptr<node> prev;
+  struct node {
+    T data;
+    int64_t freq;
+    std::shared_ptr<node> next;
+    std::weak_ptr<node> prev;
 
-        node(T data, int64_t freq, std::weak_ptr<node> prev, std::shared_ptr<node> next)
-                : data(data), freq(freq), prev(prev), next(next) {}
+    node(T data, int64_t freq, std::weak_ptr<node> prev, std::shared_ptr<node> next)
+    : data(data), freq(freq), prev(prev), next(next) {}
 
-        void add_freq() { freq++; }
-        void reset_freq() { freq = 1; }
-    };
+    void add_freq() { freq++; }
+    void reset_freq() { freq = 1; }
+  };
 
-    std::shared_ptr<node> head;
-    node *tail;
-    uint64_t size;
+  std::shared_ptr<node> head;
+  node *tail;
+  uint64_t size;
 
-    /**
+  /**
      * @brief Updates the order of nodes in the frequency list based on their frequencies.
      *
      * This function updates the order of nodes in the frequency list based on their frequencies.
@@ -195,9 +195,9 @@ private:
      * If the previous node exists, it becomes the new current node.
      * If the previous node does not exist, the traversal stops.
      */
-    void update_list_sequence();
+  void update_list_sequence();
 
-    /**
+  /**
      * @brief Swaps the positions of two nodes in the frequency list.
      *
      * If either of the nodes is null or they are not adjacent to each other, the function does nothing.
@@ -207,194 +207,194 @@ private:
      * @param a The first node to be swapped.
      * @param b The second node to be swapped.
      */
-    void swap_nodes(std::shared_ptr<node> a, std::shared_ptr<node> b);
+  void swap_nodes(std::shared_ptr<node> a, std::shared_ptr<node> b);
 };
 
 
 
 template<typename T>
 void frequency_list<T>::swap_nodes(std::shared_ptr<node> a, std::shared_ptr<node> b) {
-    if(a == nullptr || b == nullptr || a->next != b)
-        return;
+  if(a == nullptr || b == nullptr || a->next != b)
+    return;
 
-    // Attach b's next to a and a's prev to b
-    a->next = b->next;
-    b->prev = a->prev;
+  // Attach b's next to a and a's prev to b
+  a->next = b->next;
+  b->prev = a->prev;
 
-    // If b's next exists, attach a to its prev
-    if(b->next)
-        b->next->prev = a;
+  // If b's next exists, attach a to its prev
+  if(b->next)
+    b->next->prev = a;
 
-    // If a's prev exists(i.e., a is not the head), attach b to its next
-    if(a->prev.lock())
-        a->prev.lock()->next = b;
+  // If a's prev exists(i.e., a is not the head), attach b to its next
+  if(a->prev.lock())
+    a->prev.lock()->next = b;
 
-    // Now, attach a to b's next and b to a's prev
-    b->next = a;
-    a->prev = b;
+  // Now, attach a to b's next and b to a's prev
+  b->next = a;
+  a->prev = b;
 
-    // If the node a was the head node, update the head to b
-    if (head == a)
-        head = b;
+  // If the node a was the head node, update the head to b
+  if (head == a)
+    head = b;
 }
 
 template<typename T>
 void frequency_list<T>::update_list_sequence() {
-    auto current_node = head;
+  auto current_node = head;
 
-    while (current_node != nullptr && current_node->next != nullptr) {
-        if (current_node->freq < current_node->next->freq) {
-            swap_nodes(current_node, current_node->next);
+  while (current_node != nullptr && current_node->next != nullptr) {
+    if (current_node->freq < current_node->next->freq) {
+      swap_nodes(current_node, current_node->next);
 
-            if(current_node->prev.lock())
-                current_node = current_node->prev.lock();
+      if(current_node->prev.lock())
+        current_node = current_node->prev.lock();
 
-        } else
-            current_node = current_node->next;
-    }
+    } else
+      current_node = current_node->next;
+  }
 }
 
 template<typename T>
 void frequency_list<T>::reset_frequency() {
-    auto iterator = head;
+  auto iterator = head;
 
-    while (iterator != nullptr) {
-        iterator->reset_freq();
-        iterator = iterator->next;
-    }
+  while (iterator != nullptr) {
+    iterator->reset_freq();
+    iterator = iterator->next;
+  }
 }
 
 
 template<typename T>
 void frequency_list<T>::push_front(T data) {
-    auto Iterator = head;
+  auto Iterator = head;
 
-    // Iterating over the list to find if the same data is already present
-    while (Iterator != nullptr) {
-        if (Iterator->data == data) {
-            // if data is found, increase frequency and re-sort the list
-            Iterator->freq++;
-            update_list_sequence();
-            return;
-        }
-        Iterator = Iterator->next;
+  // Iterating over the list to find if the same data is already present
+  while (Iterator != nullptr) {
+    if (Iterator->data == data) {
+      // if data is found, increase frequency and re-sort the list
+      Iterator->freq++;
+      update_list_sequence();
+      return;
     }
+    Iterator = Iterator->next;
+  }
 
-    // if data is not found in the list, add new node at front
-    auto new_node = std::make_shared<node>(data, 1, std::weak_ptr<node>(), head);
+  // if data is not found in the list, add new node at front
+  auto new_node = std::make_shared<node>(data, 1, std::weak_ptr<node>(), head);
 
-    if (head != nullptr) {
-        head->prev = new_node;
-    }
+  if (head != nullptr) {
+    head->prev = new_node;
+  }
 
-    head = new_node;
+  head = new_node;
 
-    size++;
+  size++;
 }
 
 template<typename T>
 void frequency_list<T>::erase(T key) {
-    auto iterator = head;
+  auto iterator = head;
 
-    // special case: deleting the head node
-    if (head != nullptr && head->data == key) {
-        head = head->next;
-        if(head)
-            head->prev.reset();
-        size--;
-        return;
-    }
+  // special case: deleting the head node
+  if (head != nullptr && head->data == key) {
+    head = head->next;
+    if(head)
+      head->prev.reset();
+    size--;
+    return;
+  }
 
-    // iterate over the rest nodes
-    while (iterator != nullptr) {
-        if (iterator->data == key) {
-            // connect previous node and next node
-            if(auto sharedPrev = iterator->prev.lock()){
-                sharedPrev->next = iterator->next;
-            }
-            if (iterator->next) {
-                iterator->next->prev = iterator->prev;
-            }
-            // deleting the node by falling out of scope
-            size--;
-            return;
-        }
-        iterator = iterator->next;
+  // iterate over the rest nodes
+  while (iterator != nullptr) {
+    if (iterator->data == key) {
+      // connect previous node and next node
+      if(auto sharedPrev = iterator->prev.lock()){
+        sharedPrev->next = iterator->next;
+      }
+      if (iterator->next) {
+        iterator->next->prev = iterator->prev;
+      }
+      // deleting the node by falling out of scope
+      size--;
+      return;
     }
+    iterator = iterator->next;
+  }
 }
 
 template<typename T>
 int64_t frequency_list<T>::get_frequency(T key) {
-    auto iterator = head;
+  auto iterator = head;
 
-    while(iterator != nullptr) {
-        if(iterator->data == key) {
-            return iterator->freq;
-        }
-
-        iterator = iterator->next;
+  while(iterator != nullptr) {
+    if(iterator->data == key) {
+      return iterator->freq;
     }
 
-    return -1;
+    iterator = iterator->next;
+  }
+
+  return -1;
 }
 
 template<typename T>
 bool frequency_list<T>::search(T key) {
-    auto iterator = head;
+  auto iterator = head;
 
-    while (iterator != nullptr) {
-        if (iterator->data == key) {
-            iterator->add_freq();
-            update_list_sequence();
-            return true;
-        }
-
-        iterator = iterator->next;
+  while (iterator != nullptr) {
+    if (iterator->data == key) {
+      iterator->add_freq();
+      update_list_sequence();
+      return true;
     }
 
-    return false;
+    iterator = iterator->next;
+  }
+
+  return false;
 }
 
 template<typename T>
 void frequency_list<T>::push_back(T data) {
-    if (this->empty()) {
-        head = std::make_shared<node>(data, 1, std::weak_ptr<node>(), nullptr);
-        return;
-    }
+  if (this->empty()) {
+    head = std::make_shared<node>(data, 1, std::weak_ptr<node>(), nullptr);
+    return;
+  }
 
-    auto iterator = head;
-    while (iterator) {
-        if (iterator->data == data) {
-            iterator->add_freq();
-            update_list_sequence();
-            return;
-        }
-        if (!iterator->next)
-            break;
-        iterator = iterator->next;
+  auto iterator = head;
+  while (iterator) {
+    if (iterator->data == data) {
+      iterator->add_freq();
+      update_list_sequence();
+      return;
     }
+    if (!iterator->next)
+      break;
+    iterator = iterator->next;
+  }
 
-    auto newNode = std::shared_ptr<node>(new node(data, 1, iterator, nullptr));
-    if (iterator) iterator->next = newNode;
-    size++;
+  auto newNode = std::shared_ptr<node>(new node(data, 1, iterator, nullptr));
+  if (iterator) iterator->next = newNode;
+  size++;
 }
 
 
 template<typename T>
 class frequency_list<T>::Iterator {
 private:
-    std::shared_ptr<node> current;
+  std::shared_ptr<node> current;
 public:
-    /**
+  /**
      * @brief Iterator class for a linked list.
      *
      * The Iterator class provides an iterator for traversing a linked list.
      */
-    explicit Iterator(const std::shared_ptr<node> &ptr) noexcept
-        : current(ptr) {}
+  explicit Iterator(const std::shared_ptr<node> &ptr) noexcept
+  : current(ptr) {}
 
 
-    /**
+  /**
      * @brief Assignment operator for the Iterator class.
      *
      * It assigns the value of the given shared pointer to the current node of the iterator
@@ -403,12 +403,12 @@ public:
      * @param ptr The shared pointer to assign.
      * @return A reference to the updated iterator.
      */
-    Iterator &operator=(const std::shared_ptr<node> &ptr) {
-        current = ptr;
-        return *this;
-    }
+  Iterator &operator=(const std::shared_ptr<node> &ptr) {
+    current = ptr;
+    return *this;
+  }
 
-    /**
+  /**
      * @brief Pre-increment operator for the Iterator class.
      *
      * This operator overloads the pre-increment operator (++).
@@ -416,14 +416,14 @@ public:
      *
      * @return A reference to the updated iterator.
      */
-    Iterator &operator++() {
-        if(current) {
-            current = current->next;
-        }
-        return *this;
+  Iterator &operator++() {
+    if(current) {
+      current = current->next;
     }
+    return *this;
+  }
 
-    /**
+  /**
      * @brief Post-increment operator for the Iterator class.
      *
      * This operator overloads the post-increment operator (++).
@@ -431,13 +431,41 @@ public:
      *
      * @return A copy of the iterator before the increment.
      */
-    Iterator operator++(int) {
-        Iterator tmp = *this;
-        ++*this;
-        return tmp;
-    }
+  Iterator operator++(int) {
+    Iterator tmp = *(this);
+    ++*(this);
+    return tmp;
+  }
 
-    /**
+  /**
+   *@brief Pre-increment -- operator for the Iterator class.
+   *This operator overloads the pre-increment operator (--).
+   * It increments the iterator to the next node and returns a reference to the updated iterator.
+   *
+   * @return Iterator&
+   *
+   */
+  Iterator & operator --(){
+    if(current){
+      current = current -> prev;
+    }
+    return *(this);
+  }
+  
+  /**
+   *@brief Post-increment --operator for the Iterator class.
+   *This operator overloads the post-increment operator (--).
+   * It increments the iterator to the next node and returns a copy of the iterator before the increment.
+   *
+   *@return Iterator
+   */
+  Iterator operator --(int){
+    Iterator tmp = *(this);
+    --*(this);
+    return tmp;
+  }
+
+  /**
      * @brief Equality operator for the Iterator class.
      *
      * This operator checks if two iterators are equal by comparing their current nodes.
@@ -445,9 +473,9 @@ public:
      * @param it The other Iterator object to compare with.
      * @return true if the iterators are equal, false otherwise.
      */
-    bool operator==(const Iterator &it) { return it.current == this->current; }
+  bool operator==(const Iterator &it) { return it.current == this->current; }
 
-    /**
+  /**
      * @brief Inequality operator for the Iterator class.
      *
      * This operator checks if two iterators are not equal by comparing their current nodes.
@@ -455,14 +483,14 @@ public:
      * @param it The other Iterator object to compare with.
      * @return true if the iterators are not equal, false otherwise.
      */
-    bool operator!=(const Iterator &it) { return it.current != this->current; }
+  bool operator!=(const Iterator &it) { return it.current != this->current; }
 
-    /**
+  /**
      * @brief Dereference operator for the Iterator class.
      *
      * @return The data pointed to by the current node.
      */
-    T operator*() { return current->data; }
+  T operator*() { return current->data; }
 
 };
 
