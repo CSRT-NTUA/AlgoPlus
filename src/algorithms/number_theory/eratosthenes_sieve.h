a20e01285921bc8ecd6823a3c7c08cd16c34c50f
Stavros Avramidis <stavros9899@gmail.com>
2024-04-13 22:00:23 +0300

    Optimize SoE
    - Change return type to std::vector<uint8_t> to avoid performance regression of vector<bool>.
    - Change input & indexes type to size_t

--- src/algorithms/number_theory/eratosthenes_sieve.h
+++ src/algorithms/number_theory/eratosthenes_sieve.h
@@ -6,21 +6,23 @@
 #include <assert.h>
 #include <iostream>
 #include <vector>
+
+#include <cinttypes>
 #endif
 
 /**
  * @brief eratosthenes sieve function(sieve of eratosthenes)
  *
  * @param n upper bound
- * @return std::vector<bool> if arr[i] = 1 then i is prime
+ * @return std::vector<std::uint8_t> if arr[i] = 1 then i is prime
  */
-std::vector<bool> soe(int64_t n) {
+std::vector<std::uint8_t> soe(std::size_t n) {
   assert(n != 0);
-  std::vector<bool> prime(n + 1, true);
-  for (int64_t p = 2; p * p <= n; ++p) {
+  std::vector<std::uint8_t> prime(n + 1, 1);
+  for (std::size_t p = 2; p * p <= n; ++p) {
     if (prime[p]) {
-      for (int64_t i = p * p; i <= n; i += p) {
-        prime[i] = false;
+      for (std::size_t i = p * p; i <= n; i += p) {
+        prime[i] = 0;
       }
     }
   }
