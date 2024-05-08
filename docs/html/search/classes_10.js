8a8a9fad21d42d3a2780b5830d59d2deac53f243
Spiros Maggioros <spirosastro@gmail.com>
2024-05-08 20:26:52 +0300

    Fixed polynomial regression

--- docs/html/search/classes_10.js
+++ docs/html/search/classes_10.js
@@ -7,9 +7,10 @@ var searchData=
   ['plot_5fimpl_3c_20std_3a_3afalse_5ftype_20_3e_4',['plot_impl&lt; std::false_type &gt;',['../structmatplotlibcpp_1_1detail_1_1plot__impl_3_01std_1_1false__type_01_4.html',1,'matplotlibcpp::detail']]],
   ['plot_5fimpl_3c_20std_3a_3atrue_5ftype_20_3e_5',['plot_impl&lt; std::true_type &gt;',['../structmatplotlibcpp_1_1detail_1_1plot__impl_3_01std_1_1true__type_01_4.html',1,'matplotlibcpp::detail']]],
   ['pluralise_6',['pluralise',['../struct_catch_1_1pluralise.html',1,'Catch']]],
-  ['position_5ft_7',['position_t',['../structdetail_1_1position__t.html',1,'detail']]],
-  ['predicatematcher_8',['PredicateMatcher',['../class_catch_1_1_matchers_1_1_generic_1_1_predicate_matcher.html',1,'Catch::Matchers::Generic']]],
-  ['primitive_5fiterator_5ft_9',['primitive_iterator_t',['../classdetail_1_1primitive__iterator__t.html',1,'detail']]],
-  ['priority_5ftag_10',['priority_tag',['../structdetail_1_1priority__tag.html',1,'detail']]],
-  ['priority_5ftag_3c_200_20_3e_11',['priority_tag&lt; 0 &gt;',['../structdetail_1_1priority__tag_3_010_01_4.html',1,'detail']]]
+  ['polynomial_5fregression_7',['polynomial_regression',['../classpolynomial__regression.html',1,'']]],
+  ['position_5ft_8',['position_t',['../structdetail_1_1position__t.html',1,'detail']]],
+  ['predicatematcher_9',['PredicateMatcher',['../class_catch_1_1_matchers_1_1_generic_1_1_predicate_matcher.html',1,'Catch::Matchers::Generic']]],
+  ['primitive_5fiterator_5ft_10',['primitive_iterator_t',['../classdetail_1_1primitive__iterator__t.html',1,'detail']]],
+  ['priority_5ftag_11',['priority_tag',['../structdetail_1_1priority__tag.html',1,'detail']]],
+  ['priority_5ftag_3c_200_20_3e_12',['priority_tag&lt; 0 &gt;',['../structdetail_1_1priority__tag_3_010_01_4.html',1,'detail']]]
 ];
