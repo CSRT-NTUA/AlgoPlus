a691b157244bafc22e79c5b05eddab652625e564
Sahil Verma <sahilverma0696@gmail.com>
2024-07-03 18:13:09 +0530

    Added lowe_bound & upper_bound functions in binary search

--- src/algorithms/searching/binary_search.h
+++ src/algorithms/searching/binary_search.h
@@ -34,4 +34,62 @@ int64_t bin_search(std::vector<T> arr, int64_t left, int64_t right, T x) {
   return -1; // element not found
 }
 
+/**
+ * @brief lower bound search function
+ *
+ * @param arr the vector
+ * @param left the left pointer
+ * @param right the right pointer
+ * @param x the element we want to search
+ * @return int64_t the first index of the element in the vector, if it doesn't exist
+ * then it returns -1
+ */
+template <typename T>
+int64_t lower_bound(std::vector<T> arr, int64_t left, int64_t right, T x) {
+  int64_t mid = 0;
+  int64_t result =-1;
+  while (left <= right) {
+    mid = left + (right - left) / 2;
+    if (arr[mid] == x) {
+      result = mid;             // element found, make compression on left
+      right = mid-1;
+    }
+    else if (arr[mid] > x) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return result; // element not found
+}
+
+/**
+ * @brief upper bound search function
+ *
+ * @param arr the vector
+ * @param left the left pointer
+ * @param right the right pointer
+ * @param x the element we want to search
+ * @return int64_t the first last of the element in the vector, if it doesn't exist
+ * then it returns -1
+ */
+template <typename T>
+int64_t upper_bound(std::vector<T> arr, int64_t left, int64_t right, T x) {
+  int64_t mid = 0;
+  int64_t result =-1;
+  while (left <= right) {
+    mid = left + (right - left) / 2;
+    if (arr[mid] == x) {
+      result = mid;             // element found, make compression on left
+      left = mid+1;
+    }
+    else if (arr[mid] < x) {
+      left = mid+1;
+    } else {
+      right = mid-1;
+    }
+  }
+  return result; // element not found
+}
+
 #endif
--- tests/algorithms/searching/binary_search.cc
+++ tests/algorithms/searching/binary_search.cc
@@ -7,3 +7,18 @@ TEST_CASE("testing binary search") {
   REQUIRE(bin_search(v, 0, v.size() - 1, 30) == 3);
   REQUIRE(bin_search(v, 0, v.size() - 1, -20) == -1);
 }
+
+TEST_CASE("testing lower_bound search") {
+  std::vector<int> v = {10, 10, 20, 30, 30, 30, 30, 100};
+  REQUIRE(lower_bound(v, 0, v.size() - 1, 10) == 0);
+  REQUIRE(lower_bound(v, 0, v.size() - 1, 30) == 3);
+  REQUIRE(lower_bound(v, 0, v.size() - 1, -20) == -1);
+}
+
+
+TEST_CASE("testing upper_bound search") {
+  std::vector<int> v = {10, 10, 20, 30, 30, 30, 30, 100};
+  REQUIRE(upper_bound(v, 0, v.size() - 1, 10) == 1);
+  REQUIRE(upper_bound(v, 0, v.size() - 1, 30) == 6);
+  REQUIRE(upper_bound(v, 0, v.size() - 1, -20) == -1);
+}
