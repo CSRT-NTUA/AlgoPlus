ff481ff8e73494c4d7c900d631a0ebced65ba65a
Spiros Maggioros <spirosastro@gmail.com>
2024-07-16 13:24:16 +0300

    Some additions, nothing yet

---
+++ python/bst.py
@@ -0,0 +1,6 @@
+from algoplus_py import *
+
+a = bst_int([])
+a.insert(10)
+a.insert(20)
+print(a.inorder())
--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,26 @@
 from setuptools import setup, find_packages
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 VERSION = '1.0.1'
 DESCRIPTION = 'AlgoPlus is a C++17 library for complex data structures and algorithms'
 
+
+ext_modules = [
+        Pybind11Extension(
+            "tree",
+            ["classes/tree/tree.cc"],
+            define_macros=[("VERSION_INFO", VERSION)]
+            ),
+        ]
+
 setup(
-    name="AlgoPlus-python",
+    name="algoplus_py",
     version=VERSION,
     author="Spiros Maggioros and the CSRT-NTUA team",
     author_email="<spirosmag@ieee.org>",
     description=DESCRIPTION,
-    packages=find_packages()
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    python_requires=">=3.8"
 )
