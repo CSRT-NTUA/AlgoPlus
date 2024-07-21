c3c588b3b595cf49ed55c042d9a8e0009e742be8
Spiros Maggioros <spirosastro@gmail.com>
2024-07-21 22:47:27 +0300

    It works

--- python/bst.py
+++ python/bst.py
@@ -1,6 +1,6 @@
-from algoplus_py import *
+from algoplus_py import tree 
 
-a = bst_int([])
+a = tree.bst_int([])
 a.insert(10)
 a.insert(20)
 print(a.inorder())
--- python/setup.py
+++ python/setup.py
@@ -1,26 +1,29 @@
 from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
+import os
+
+root = os.path.abspath(os.path.dirname(__file__))
 
 VERSION = '1.0.1'
 DESCRIPTION = 'AlgoPlus is a C++17 library for complex data structures and algorithms'
 
-
 ext_modules = [
-        Pybind11Extension(
-            "tree",
-            ["classes/tree/tree.cc"],
-            define_macros=[("VERSION_INFO", VERSION)]
-            ),
-        ]
+    Pybind11Extension(
+        "algoplus_py.tree",
+        ["classes/tree/tree.cc"],
+        define_macros=[("VERSION_INFO", VERSION)],
+    ),
+]
 
 setup(
     name="algoplus_py",
     version=VERSION,
     author="Spiros Maggioros and the CSRT-NTUA team",
-    author_email="<spirosmag@ieee.org>",
+    author_email="spirosmag@ieee.org",
     description=DESCRIPTION,
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
-    python_requires=">=3.8"
+    packages=find_packages(),
+    python_requires=">=3.8",
 )
