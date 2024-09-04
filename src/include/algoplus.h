1a80e3ea85f8d23118748a6d91889cd1022a33fc
Spiros Maggioros <spirosastro@gmail.com>
2024-09-04 10:38:16 +0300

    add missing headers to algoplus.h

--- src/include/algoplus.h
+++ src/include/algoplus.h
@@ -6,42 +6,78 @@
 #include "../algorithms/dynamic_programming/kadane.h"
 #include "../algorithms/dynamic_programming/lcs.h"
 #include "../algorithms/dynamic_programming/lis.h"
+
 #include "../algorithms/number_theory/eratosthenes_sieve.h"
 #include "../algorithms/number_theory/gcd.h"
 #include "../algorithms/number_theory/mersenne_primes.h"
+
+#include "../algorithms/searching/bfs.h"
 #include "../algorithms/searching/binary_search.h"
+#include "../algorithms/searching/dfs.h"
 #include "../algorithms/searching/exponential_search.h"
+#include "../algorithms/searching/interpolation_search.h"
 #include "../algorithms/searching/jump_search.h"
 #include "../algorithms/searching/linear_search.h"
+#include "../algorithms/searching/ternary_search.h"
+
 #include "../algorithms/sorting/bubble_sort.h"
 #include "../algorithms/sorting/bucket_sort.h"
+#include "../algorithms/sorting/counting_sort.h"
 #include "../algorithms/sorting/heap_sort.h"
 #include "../algorithms/sorting/insertion_sort.h"
 #include "../algorithms/sorting/merge_sort.h"
 #include "../algorithms/sorting/quick_sort.h"
+#include "../algorithms/sorting/radix_sort.h"
 #include "../algorithms/sorting/selection_sort.h"
+
 #include "../algorithms/string/edit_distance.h"
 #include "../algorithms/string/find_and_replace.h"
 #include "../algorithms/string/kmp.h"
+#include "../algorithms/string/rabin_karp.h"
+
 #include "../classes/disjoint_set/disjoint_set.h"
 #include "../classes/graph/graph.h"
 #include "../classes/hash_table/hash_table.h"
 #include "../classes/heap/min_heap.h"
+
 #include "../classes/list/circular_linked_list.h"
 #include "../classes/list/doubly_linked_list.h"
+#include "../classes/list/frequency_list.h"
 #include "../classes/list/linked_list.h"
 #include "../classes/list/skip_list.h"
+
 #include "../classes/queue/dequeue_list.h"
 #include "../classes/stack/stack_list.h"
-#include "../classes/tree/tree.h"
+
+#include "../classes/tree/234_tree.h"
 #include "../classes/tree/avl_tree.h"
 #include "../classes/tree/bst.h"
 #include "../classes/tree/interval_tree.h"
-#include "../classes/tree/splay_tree.h"
-#include "../classes/tree/trie.h"
 #include "../classes/tree/red_black_tree.h"
+#include "../classes/tree/splay_tree.h"
+#include "../classes/tree/tree.h"
+#include "../classes/tree/trie.h"
+
 #include "../machine_learning/clustering/DBSCAN/dbscan.h"
 #include "../machine_learning/clustering/kmeans/kmeans.h"
+
+#include "../machine_learning/image/edge_detection/prewitt.h"
+#include "../machine_learning/image/edge_detection/sobel_operator.h"
+
+#include "../machine_learning/image/encoders/huffman_encoding.h"
+
+#include "../machine_learning/image/filters/average_filter.h"
+#include "../machine_learning/image/filters/median_filter.h"
+#include "../machine_learning/image/filters/sharpening_filter.h"
+#include "../machine_learning/image/morphology/operations.h"
+#include "../machine_learning/image/point_detection/laplacian_point_detect.h"
+#include "../machine_learning/image/image.h"
+
+#include "../machine_learning/loss_functions/losses.h"
+
+#include "../machine_learning/regression/linear_regression/lin_reg.h"
+#include "../machine_learning/regression/polynomial_regression/poly_reg.h"
+
 #include "../machine_learning/search_algorithms/best_first/best_first.h"
 #include "../machine_learning/search_algorithms/hill_climbing/hill_climbing.h"
 #include "../machine_learning/regression/linear_regression/lin_reg.h"
