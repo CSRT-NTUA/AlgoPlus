feaf0f2d137d633388121da2ab58971d13b6f4a3
Spiros Maggioros <spirosastro@gmail.com>
2024-09-08 17:10:58 +0300

    Changed version to cmake and some additions on test cases cmakelists

---
+++ examples/graph/test.cc
@@ -0,0 +1,20 @@
+#ifndef GRAPH_H
+#include "../../src/classes/graph/graph.h"
+#endif
+
+int main() {
+    weighted_graph<std::string> g("directed");
+    g.add_edge("Athens", "Benfica", 300);
+    g.add_edge("Athens", "Milan", 120);
+    g.add_edge("Milan", "Paris", 40);
+    g.add_edge("Paris", "Benfica", 120);
+    g.add_edge("Rome", "Malta", 110);
+    g.add_edge("Milan", "Malta", 250);
+    g.add_edge("Malta", "Berlin", 190);
+    g.add_edge("Athens", "Rome", 70);
+
+    std::vector<string> topo = g.topological_sort();
+
+    std::cout << g.shortest_path("Athens", "Malta") << '\n';
+    std::cout << g.shortest_path("Athens", "Benfica") << '\n';
+}
